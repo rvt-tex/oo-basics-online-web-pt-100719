da41dbd2dbe09f27d829facc1bd16193df18dd4b
Ruel Tingling <rvtingling@gmail.com>
2019-10-15 21:37:45 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,73 @@
+class Book
+  def initialize(title)
+    @title = title 
+  end
+  
+  def title
+    @title
+  end 
+  
+  def author= (author)
+    @author = author
+  end 
+  
+  def author
+    @author
+  end 
+  
+  def page_count= (num)
+    @page_count = num
+  end 
+  
+  def page_count
+    @page_count
+  end 
+  
+  def genre= (genre)
+    @genre = genre
+  end 
+  
+  def genre
+    @genre
+  end 
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+end 
 
+# in line one we creating a Book class and then initializing it with a title. the title is passed in as an argument
+# in line three we are creating an instance variable using the @ symbol. this variable is the argurement/(property) of that class book.
+# line six we created a title method ["getter"] for that intialize property(arguement) using the @ symbol. hence when we call the .title on a book instance it returns just that property
 
+# in line ten we created a setter method for author. setter methods are generally named like [property = "something"/ can be written as instance.property = "something"]
+# in line fourteen we created the "getter" for the property author.
+
+# in line eighteen we added a "setter" method for page count passing an arguement of num.
+
+# in line twenty two we created a "getter" method for page_count.
+
+#line twenty six we created a "setter" method for the property genre.
+
+#line thirty we created a "getter" method for the property genre.
+
+# in line thirty four we created a turn page method that uses "puts" to print a string onto the screen. [ so now we will be able to tell out book to turn the page by doing; 
+ # book = Book.new("some Title")
+ # book.turn_page
+ 
+ 
+ #class Book
+  #attr_accessor :author, :page_count, :genre
+  #attr_reader :title
+ 
+  #def initialize(title)
+    #@title = title
+  #end
+ 
+  #def turn_page
+    #puts "Flipping the page...wow, you read fast!"
+ # end
+ 
+#end
+
+# the above code from line fifty nine to seventy one is creating a class book and setting its properties using the "attr_accessor" which is the ("setter") and the attr_reader which is the ("getter")
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,71 @@
 # Make your shoe class here!
+# the :: it indicates "name spacing". In other words, it tells us that the thing on the right side of the colons is a thing defined within the context, or name space, of the thing on the left side of the colons.
+
+#class Shoe 
+ # attr_accessor :brand, :color, :size, :material, :condition
+ # attr_reader :shoe
+ 
+ # def initialize(shoe)
+ #   @shoe = shoe
+#  end
+#end
+
+
+class Shoe
+  def initialize(shoe)
+    @shoe = shoe 
+    @brand = brand 
+  end
+  
+  def shoe 
+    @shoe 
+  end 
+  
+  def brand=(brand)
+    @brand = brand
+  end 
+  
+  def brand
+    @brand
+  end 
+  
+  def color= (color)
+    @color = color
+  end 
+  
+  def color
+    @color
+  end 
+  
+  def size= (size)
+    @size = size
+  end 
+  
+  def size
+    @size 
+  end 
+  
+  def material= (material)
+    @material = material
+  end 
+  
+  def material
+    @material
+  end 
+  
+  def condition=(new)
+    @condition = new 
+  end 
+  
+  def condition
+    @condition
+  end 
+  
+  def cobble
+   puts "Your shoe is as good as new!"
+  end 
+end 
+  
+ 
+  
+  
